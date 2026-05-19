3ddc17aff2ec12293798654ae8e5c38c6ac24b7a
uptobe8 <ia.uptobe@gmail.com>
2026-05-20 01:23:33 +0200

    Add Pages app config

---
+++ config.js
@@ -0,0 +1,6 @@
+window.DIVI_MANAGER_CONFIG = {
+  appName: 'Divi Manager',
+  accessGateEnabled: true,
+  accessKeyHash: 'b753cf09b5a99cc39da22d99b8a205f28c5a1b36a34182c072cc99e12b6f8134',
+  defaultOutputPrefix: 'divi-layout'
+};
