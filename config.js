5fb1d2835398c468e42791d46003133ee0abecac
uptobe8 <ia.uptobe@gmail.com>
2026-05-20 04:30:54 +0200

    Allow simple access key while keeping hash fallback

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 window.DIVI_MANAGER_CONFIG = {
   appName: 'Divi Manager',
   accessGateEnabled: true,
+  accessKey: 'divi-manager',
   accessKeyHash: 'b753cf09b5a99cc39da22d99b8a205f28c5a1b36a34182c072cc99e12b6f8134',
   defaultOutputPrefix: 'divi-layout'
 };
